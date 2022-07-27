4586bcd51aea3dd24ac9d099884b1514bfca4d33
Ajacmac <jacob.macritchie@gmail.com>
2022-07-26 23:53:01 -0300

    added first autogenerated unit test...now to figure out what to do with it xD

--- load_balancer_test.go
+++ load_balancer_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 // func TestAddBackend(t *testing.T) {
 // 	original := serverPool
@@ -13,7 +15,24 @@ import "testing"
 // 	log.Printf("Configured server: %s\n", serverUrl)
 // }
 
-func TestSetupBackends(t *testing.T) {
-	// add subtests, maybe 10 per type, for each step
-	// How do I keep breaking out the functionality to make it more testable?
+// func TestSetupBackends(t *testing.T) {
+// 	// add subtests, maybe 10 per type, for each step
+// 	// How do I keep breaking out the functionality to make it more testable?
+// }
+
+func Test_setupBackends(t *testing.T) {
+	type args struct {
+		serverList string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			setupBackends(tt.args.serverList)
+		})
+	}
 }
