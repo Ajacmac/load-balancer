2ba6f1bff2ad6a8f9a4a8176b9940e1b24418dd9
Ajacmac <jacob.macritchie@gmail.com>
2022-07-27 23:54:23 -0300

    blah

--- load_balancer_test.go
+++ load_balancer_test.go
@@ -9,21 +9,8 @@ import (
 	"testing"
 )
 
-// func TestAddBackend(t *testing.T) {
-// 	original := serverPool
-
-// 	serverPool.AddBackend(&Backend{
-// 		URL:          serverUrl,
-// 		Alive:        true,
-// 		ReverseProxy: proxy,
-// 	})
-// 	log.Printf("Configured server: %s\n", serverUrl)
-// }
-
-// func TestSetupBackends(t *testing.T) {
-// 	// add subtests, maybe 10 per type, for each step
-// 	// How do I keep breaking out the functionality to make it more testable?
-// }
+// add subtests, maybe 10 per type, for each step
+// How do I keep breaking out the functionality to make it more testable?
 
 func Test_setupBackends(t *testing.T) {
 	type args struct {
