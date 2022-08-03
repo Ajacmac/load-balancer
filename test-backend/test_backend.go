4916cb0cf56650c61dfff68ca68f7072b31d01ae
Ajacmac <jacob.macritchie@gmail.com>
2022-08-02 22:49:20 -0300

    made plan for demo backend servers

--- test-backend/test_backend.go
+++ test-backend/test_backend.go
@@ -5,3 +5,14 @@ import "net/http"
 func main() {
 	http.Handle("/", http)
 }
+
+/*
+	TODO:
+	-setup basic static file server
+	-make docker image
+	-setup docket containers
+	-setup test function to invoke the docker containers
+	-have test function create the index.html file for each container,
+	each html file only containing a number for identifying which backend responded
+
+*/
